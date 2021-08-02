943f3e44d241674b247028326a4e3915d9e364b9
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-02 23:26:49 +0800

    feat: init server.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+import { createServer } from 'http';
+
+const PORT = process.argv[2];
+
+const handleRequest = (req, res) => {
+  if (req.method === 'GET') {
+    console.log('This is a GET');
+  } else {
+    res.writeHead(405, {});
+    res.end('Method Not Allowed: Only GET requests are accepted by the resource.');
+  }
+};
+
+const init = () => {
+  if (Number.isNaN(Number(PORT)) || Number(PORT) < 0 || Number(PORT) >= 65536) {
+    console.log('Please enter a valid port number >=0 and < 65536.');
+  } else {
+    createServer(handleRequest).listen(Number(PORT));
+  }
+};
+
+init();
